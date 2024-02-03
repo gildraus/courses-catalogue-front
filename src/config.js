bd504590e9ddcb0347820f41596ce60a50c636da
gildrausNV <nikola.vujcic.001@gmail.com>
2024-02-03 13:01:55 +0100

    server_name change

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const server_name = "courses-catalogue-back.vercel.app";// if deployed
+const server_name = "https://courses-catalogue-back.vercel.app"; // if deployed
 // const server_name = "http://localhost:3001"; //if local
 
 export default server_name;
