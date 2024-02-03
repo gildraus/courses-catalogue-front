8c84a1fc49f82b4e11817afa568c393dd814ebb6
gildrausNV <nikola.vujcic.001@gmail.com>
2024-02-03 12:52:20 +0100

    protocol change

--- src/config.js
+++ src/config.js
@@ -1,4 +1,8 @@
-const server_name = "courses-catalogue-back.vercel.app";// if deployed
-// const server_name = "http://localhost:3001"; //if local
+// Use the correct protocol based on the environment
+const protocol = window.location.protocol;
+const server_name =
+  process.env.NODE_ENV === "production"
+    ? `${protocol}//courses-catalogue-back.vercel.app`
+    : "http://localhost:3001";
 
 export default server_name;
